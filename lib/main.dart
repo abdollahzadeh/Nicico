374d6f2a8b2210da234fb56b8ae879e2dc8da7d4
abdollahzadeh <33484652+abdollahzadeh@users.noreply.github.com>
2019-01-22 14:31:21 +0330

    change tabbar view and icon

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
      return new SplashScreen(seconds: 14,navigateAfterSeconds:new AfterSplash(),
        backgroundColor: Colors.white,
-       image:new Image.asset('assets/images/nicico-logo.png'),
+       image:new Image.asset('assets/images/logo.jpg',fit: BoxFit.fill),
        title: new Text(''),
        photoSize: 100.0
 
@@ -41,19 +41,35 @@ class AfterSplash extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("معاونت"),
-          automaticallyImplyLeading: false
-      ),
-      body: new Center(
-        child: new Text("Succeeded!",
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0
-          ),),
+   return new DefaultTabController(length: 8,initialIndex: 1, child:
+        Scaffold(appBar:new AppBar(
+          title: new Text('مجتمع مس سرچشمه - معاونت توسعه و برنامه ریزی',style: new TextStyle(fontFamily: 'Vazir'),),
+          centerTitle: true,
+          leading: new Icon(Icons.arrow_back),
+          actions: <Widget>[
+            new IconButton(icon:new Icon(Icons.menu), onPressed:(){Scaffold.of(context).openDrawer();})
+          ],
+          bottom: new TabBar(labelStyle: TextStyle(fontFamily: 'Vazir'),tabs:[
+            new Tab(text: 'پروژه ها',),
+            new Tab(text: 'اخبار',),
+            new Tab(text: 'مزایده ها',),
+            new Tab(text: 'مناقصه ها',),
+            new Tab(text: 'فروشگاه',),
 
-      ),
+          ],isScrollable: true,indicatorColor: Colors.blue,),
+        ),
+
+          endDrawer:new Drawer(
+             child: new Text('fff')
+        ), 
+         body:
+         TabBarView(children:<Widget> [
+          new Text('ffff'),
+          new Text('oppp'),
+          new Text('mnnmn'),
+          new Text('099090'),
+          new Text('ytty'),
+        ]),)
     );
   }
 }
